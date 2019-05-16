cb08e65d0aac9ad7e9894a46e8a5f9ea9f3e1b44
mchausse <maxime.chausse@hotmail.ca>
2019-05-16 13:11:36 -0400

    creation du soapParamount fonctionnel

---
+++ SoapParamount/ParamountSoap/src/main/java/com/ParamountSoap/DAO/ConnexionDB.java
@@ -0,0 +1,25 @@
+package com.ParamountSoap.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnexionDB {
+
+    private static String url = "jdbc:mysql://localhost/catalogueparamount?serverTimezone=EST";
+    private static String user = "root";
+    private static String passwd = "root";
+    private static Connection connect;
+
+    public static Connection getInstance(){
+        if(connect == null){
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connect = DriverManager.getConnection(url, user, passwd);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return connect;
+    }
+
+}
