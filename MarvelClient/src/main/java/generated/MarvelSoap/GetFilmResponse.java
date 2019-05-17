85db300b1c3e5f10effaff170e7c5da1ef75684a
Slaverdure06 <Slaverdure06@gmail.com>
2019-05-17 17:40:43 -0400

    Ajout du Soap de Marvel

---
+++ MarvelClient/src/main/java/generated/MarvelSoap/GetFilmResponse.java
@@ -0,0 +1,62 @@
+
+package generated.MarvelSoap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour getFilmResponse complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="getFilmResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://Service.MarvelSoap.com/}film" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getFilmResponse", propOrder = {
+    "_return"
+})
+public class GetFilmResponse {
+
+    @XmlElement(name = "return")
+    protected Film _return;
+
+    /**
+     * Obtient la valeur de la propriété return.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Film }
+     *     
+     */
+    public Film getReturn() {
+        return _return;
+    }
+
+    /**
+     * Définit la valeur de la propriété return.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Film }
+     *     
+     */
+    public void setReturn(Film value) {
+        this._return = value;
+    }
+
+}
---
+++ MarvelClient/src/main/java/generated/MarvelSoap/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://Service.MarvelSoap.com/")
+package generated.MarvelSoap;
